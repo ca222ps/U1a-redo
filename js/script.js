5eb04b338956fa90ab8fb4ad922671f489794bdc
Claudia Gutenberg <ca222ps@student.lnu.se>
2024-11-27 15:00:49 +0100

    1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* Uppgift U1a */
+
+// Globala variabler
+let optionsDialog;	    // Element för inställningsdialog
+let die1Elem, die2Elem,
+    die3Elem;           // Element för tärningarna
+let rollElem;           // Element för utskrift av antal omkast som återstår
+let resElem;	        // Element för resultat
+let stopBtn;            // Knapp för att stanna
+let maxNrOfRolls = 3;   // Valt max antal omkast av tärningar
+let nrOfRolls = 0;      // Antal omkast som återstår
+// --------------------------------------------------
+// Ta fram referenser till element i gränssnittet och lägg till händelselyssnare.
+function init() {
+	
+} // Slut init
+window.addEventListener("load", init);
+// --------------------------------------------------
